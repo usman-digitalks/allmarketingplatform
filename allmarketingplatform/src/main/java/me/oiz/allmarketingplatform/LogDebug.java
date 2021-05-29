e36c0b55ca0465a40a94a645b545bfede6f9f182
usman-digitalks <77572773+usman-digitalks@users.noreply.github.com>
2021-05-29 20:39:22 +0400

    first commit

---
+++ allmarketingplatform/src/main/java/me/oiz/allmarketingplatform/LogDebug.java
@@ -0,0 +1,15 @@
+package me.oiz.allmarketingplatform;
+
+import android.util.Log;
+
+public class LogDebug {
+
+    private static final String TAG = "ALL_Marketing_Platform_APP";
+
+    public static void d(String message){
+
+        Log.d(TAG, message);
+
+    }
+
+}
