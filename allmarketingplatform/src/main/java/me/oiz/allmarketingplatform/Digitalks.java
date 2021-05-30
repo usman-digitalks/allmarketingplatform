ae973a34c875a7388864e7c8a7309c5e4097e546
usman-digitalks <77572773+usman-digitalks@users.noreply.github.com>
2021-05-30 18:58:11 +0400

    branch added

--- allmarketingplatform/src/main/java/me/oiz/allmarketingplatform/Digitalks.java
+++ allmarketingplatform/src/main/java/me/oiz/allmarketingplatform/Digitalks.java
@@ -5,6 +5,14 @@ import android.os.Bundle;
 
 import com.google.firebase.analytics.FirebaseAnalytics;
 
+import io.branch.indexing.BranchUniversalObject;
+import io.branch.referral.util.BRANCH_STANDARD_EVENT;
+import io.branch.referral.util.BranchContentSchema;
+import io.branch.referral.util.BranchEvent;
+import io.branch.referral.util.ContentMetadata;
+import io.branch.referral.util.CurrencyType;
+import io.branch.referral.util.ProductCategory;
+
 public class Digitalks {
 
     public static FirebaseAnalytics mFirebaseAnalytics;
@@ -17,6 +25,34 @@ public class Digitalks {
         bundle.putString(FirebaseAnalytics.Param.ITEM_NAME, "usman");
         bundle.putString(FirebaseAnalytics.Param.CONTENT_TYPE, "image");
         mFirebaseAnalytics.logEvent(FirebaseAnalytics.Event.SELECT_CONTENT, bundle);
+
+
+        BranchUniversalObject buo = new BranchUniversalObject()
+                .setCanonicalIdentifier("myprod/1234")
+                .setCanonicalUrl("https://test_canonical_url")
+                .setTitle("test_title")
+                .setContentMetadata(
+                        new ContentMetadata()
+                                .setPrice(10.0, CurrencyType.USD)
+                                .setProductBrand("test_prod_brand")
+                                .setProductCategory(ProductCategory.APPAREL_AND_ACCESSORIES)
+                                .setProductName("test_prod_name")
+                                .setProductCondition(ContentMetadata.CONDITION.EXCELLENT)
+                                .setProductVariant("test_prod_variant")
+                                .setQuantity(1.5)
+                                .setSku("test_sku")
+                                .setContentSchema(BranchContentSchema.COMMERCE_PRODUCT));
+
+        new BranchEvent(BRANCH_STANDARD_EVENT.ADD_TO_CART)
+                .setAffiliation("test_affiliation")
+                .setCustomerEventAlias("my_custom_alias")
+                .setCurrency(CurrencyType.USD)
+                .setDescription("Customer added item to cart")
+                .setShipping(0.0)
+                .setTax(9.75)
+                .setRevenue(1.5)
+                .addContentItems(buo)
+                .logEvent(context);
 
     }
 
